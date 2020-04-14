235b157b6acb1ae15d96124f4d45dd98f057fcf5
Anton1912 <59968195+Anton1912@users.noreply.github.com>
2020-04-14 14:20:11 +0300

    Update cross_product.py

--- cross_product.py
+++ cross_product.py
@@ -1,9 +1,10 @@
 from model_db import*
-import csv
 from model_csv import csv_dict_writer
 
-cross_product = []
-def get_cross_product()
-    Item_Character.select(Item_Character.product_)\
-        .where(Item_Character.character_ == "Серія").execute()
+cross_product = "cpd.csv"
 
+main_id = (Item_Character.select(Item_Character.product_, Item_Character.name_item).join(Product)
+           .where(Item_Character.character_ == "Серія"))
+
+for product in main_id:
+    pass
